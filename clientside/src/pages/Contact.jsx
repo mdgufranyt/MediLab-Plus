a612477fceea216dfe3aaba84d24b00ec2a2a6ba
mdgufranyt <gufranbareilly@gmail.com>
2026-05-14 00:30:36 +0530

    Update Contact.jsx with MediLab+ information (New Delhi office and contact details)

--- clientside/src/pages/Contact.jsx
+++ clientside/src/pages/Contact.jsx
@@ -20,13 +20,13 @@ const Contact = () => {
         <div className="flex flex-col justify-center items-start gap-6">
           <p className="font-semibold text-lg text-gray-600">OUR OFFICE</p>
           <p className="text-gray-500">
-            54709 Willms Station <br /> Suite 350, Washington, USA
+            New Delhi <br /> India
           </p>
           <p className="text-gray-500">
-            Tel: (415) 555‑0132 <br /> Email: elyseniyibizi502@gmail.com
+            Tel: (+91) 9876543210 <br /> Email: MediLabPlus@gmail.com
           </p>
           <p className="font-semibold text-lg text-gray-600">
-            CAREERS AT PRESCRIPTO
+            CAREERS AT MEDILAB PLUS
           </p>
           <p className="text-gray-500">
             Learn more about our teams and job openings.
@@ -41,3 +41,4 @@ const Contact = () => {
 };
 
 export default Contact;
+
